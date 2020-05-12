f49afe5066c0ddc38e7c73c6e94be3bf1e44547c
tm-mlt <artyom-multan@rambler.ru>
2020-05-13 02:17:57 +0300

    'initial'

---
+++ webpack.dev.js
@@ -0,0 +1,129 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs');
+const glob = require('glob');
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  dist: path.join(__dirname, 'dist'),
+  pages: path.join(__dirname, 'src/pages'),
+};
+
+const PAGES = glob
+  .sync(`${PATHS.pages}/**/*.pug`)
+  .map(file => path.relative(PATHS.pages, file));
+console.log(PAGES);
+
+module.exports = {
+  entry: {
+    main: `${PATHS.src}/index.js`
+  },
+
+  output: {
+    chunkFilename: '[name].[chunkhash].js',
+    filename: '[name].[chunkhash].js'
+  },
+
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
+
+        options: {
+          plugins: ['syntax-dynamic-import'],
+
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
+
+        test: /\.js$/
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.(scss|css)$/,
+
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+
+  mode: 'development',
+
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
+
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  },
+
+  plugins: [
+    ...PAGES.map(page => {
+      const isProjects = page.search(/projects/) > -1;
+      const basename = path.basename(page);
+      const filename = isProjects ? `projects/${basename}` : basename;
+      return new HtmlWebpackPlugin({
+        template: path.join(PATHS.pages, isProjects ? 'projects' : '', basename),
+        filename: filename.replace(/\.pug/, '.html'),
+      });
+    })
+  ],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080
+  }
+};
