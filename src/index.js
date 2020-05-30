8c9decb8b008fc588b09b29bf09d01c7b7931935
tm-mlt <artyom-multan@rambler.ru>
2020-05-30 11:00:33 +0300

    feat(js): add active class on 'contacts' click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './styles/index.scss';
 
-console.log('test');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const activeClass = 'active';
+  const contacts = document.querySelector('[href="/about#contacts"]');
+  const active = document.querySelector('.header__link.active');
+
+  function activateLink() {
+    active.classList.remove(activeClass);
+    contacts.classList.add(activeClass);
+  }
+
+  if(location.hash === "#contacts") {
+    activateLink();
+  } else {
+    contacts.addEventListener('click', activateLink);
+  }
+});
